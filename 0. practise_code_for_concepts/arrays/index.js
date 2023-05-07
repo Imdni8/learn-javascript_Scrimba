2699f00a2d174c6ea0d7429f44737270df8b224a
Imdni8 <tousif.rahaman@gmail.com>
2023-05-07 22:52:49 +0530

    kind of completed blackjack

---
+++ 0. practise_code_for_concepts/arrays/index.js
@@ -0,0 +1,10 @@
+let skill = [
+    "UX design",
+    "interaction design",
+    "User research",
+    "Rapid prototyping"
+]
+
+let intro = ["Tousif", 28, false]
+
+skill.forEach(x => console.log(Element))
--- 3. Build a Blackjack game/my solution/index.js
+++ 3. Build a Blackjack game/my solution/index.js
@@ -4,10 +4,11 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   }
 
-var firstCard = getRandomIntInclusive(2,11)
-var secondCard = getRandomIntInclusive(2,11)
+var firstCard
+var secondCard
+let cards = []
 
-var sum = firstCard + secondCard
+var sum = 0
 
 let hasBlackJack = false
 let isAlive = true
@@ -15,20 +16,47 @@ let isAlive = true
 let Msg = ""
 
 // Blackjack logic
-function startGame() {
+function gameLogic() {
   if (sum <= 20) {
     Msg = "Do you want to draw another card?"
   } else if (sum === 21) {
-    Msg = "Wohoo! You have Blackjack"
+    Msg = "Wohoo! You have Blackjack. Another game?"
     hasBlackJack = true
   } else {
-    Msg = "You're out of the game"
+    Msg = "You're out of the game. Start a new game?"
     isAlive = false
   }
 
+  // player is out and ask to start a new game
+  /* Ask to start new game when - 
+  1. player has blackjack 
+  (or)
+  2. player is out
+  */
+  if (hasBlackJack === true || isAlive === false) {
+    document.getElementById("newCardBtn").style.display = "none"
+  }
+
   document.getElementById("Msg-el").textContent = Msg
-  document.getElementById("cards").textContent = firstCard + ", " + secondCard
+  document.getElementById("cards").textContent = cards
   document.getElementById("sum").textContent = sum
 }
 
-console.log(firstCard, secondCard, sum)
+function startGame() {
+  firstCard = getRandomIntInclusive(2,11)
+  secondCard = getRandomIntInclusive(2,11)
+  cards.push(firstCard)
+  cards.push(secondCard)
+  sum = firstCard + secondCard
+
+  gameLogic()
+}
+
+function newCard() {
+  cards.push(getRandomIntInclusive(2,11))
+  
+  //calculating sum of the cards array
+  sum += Number(cards.slice(-1))
+
+  gameLogic()
+}
