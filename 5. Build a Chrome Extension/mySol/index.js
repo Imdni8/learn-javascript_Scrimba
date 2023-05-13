4e3ef45ffa79d27be534a07558ff13b86770e590
Imdni8 <tousif.rahaman@gmail.com>
2023-05-13 12:01:41 +0530

    minor update

--- 5. Build a Chrome Extension/mySol/index.js
+++ 5. Build a Chrome Extension/mySol/index.js
@@ -3,12 +3,23 @@ let myLeads = []
 // targetting DOM elements
 const inputField = document.getElementById("input-field")
 const saveBtn = document.getElementById("save-btn")
-const feedback = document.getElementById("feedback")
+const savedLeads = document.querySelector(".leads")
+const leadNum = document.querySelector("#lead-num")
 
 
-//push lead to myLeads array when "SAVE INPUT" btn is clicked
 saveBtn.addEventListener("click", function() {
+    //push lead to myLeads array when "SAVE INPUT" btn is clicked
     myLeads.push(inputField.value)
-    inputField.value = ""
     console.log(myLeads)
+
+    //render lead stats
+    // for (let i=0; i<myLeads.length; i++) {
+    //     savedLeads.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // }
+
+    savedLeads.innerHTML += "<li>" + myLeads.splice[-1] + "</li>"
+
+    //reset the field
+    inputField.value = ""
 })
+
