67887eeb85864a20c7358fc77f385191f21b9428
Imdni8 <tousif.rahaman@gmail.com>
2023-05-22 23:10:37 +0530

    leads persist

--- 5. Build a Chrome Extension/mySol/index.js
+++ 5. Build a Chrome Extension/mySol/index.js
@@ -7,15 +7,41 @@ const saveBtn = document.getElementById("save-btn")
 const leadCount = document.querySelector("#lead-count")
 const renderLeads = document.querySelector(".leads")
 
+//render out localstorage if there is data
+if (localStorage.getItem("Leads") !== null) {
+    myLeads =  JSON.parse(localStorage.getItem("Leads"))
 
+    leadCount.textContent = myLeads.length
+
+    let listItems = ""
+    for (let i=0; i<myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href= "${myLeads[i]}">
+            ${myLeads[i]}
+            </a>
+        </li>
+        `
+    }
+
+    renderLeads.innerHTML = listItems
+}
+
+
+//Save btn function
 saveBtn.addEventListener("click", function() {
-    //push lead to myLeads array when "SAVE INPUT" btn is clicked
+    //push lead to myLeads array on "SAVE INPUT" btn click
     myLeads.push(inputField.value)
     console.log(myLeads)
 
+    //reset the field
+    inputField.value = ""
+
     //increment count (number of leads in MyLeads) - in DOM
-    count += 1
-    leadCount.textContent = count
+    leadCount.textContent = myLeads.length
+
+    //storing myleads to localstorage
+    localStorage.setItem("Leads", JSON.stringify(myLeads))
 
     //construct the list
     let listItems = ""
@@ -32,8 +58,5 @@ saveBtn.addEventListener("click", function() {
     console.log(listItems)
 
     //render the list - in DOM
-    renderLeads.innerHTML = listItems
-
-    //reset the field
-    inputField.value = ""
+    renderLeads.innerHTML = listItems    
 })
